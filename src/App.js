529961e9360135073ee4eefa04185d1375a42f07
beniiiiii <56236600+beniiiiii@users.noreply.github.com>
2022-02-02 15:03:51 +0100

    Installed react-router@8 and maked component for: all pages, footer and header, now we can start working in our branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { Route, Routes } from 'react-router';
 import './styles/AppStyle/AppStyle.css'
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import NewCarsPage from './components/NewCarsPage';
+import UsedCarsPage from './components/UsedCarsPage';
+import ComparePage from './components/ComparePage';
+import SellPage from './components/SellPage';
+import RegisterPage from './components/RegisterPage';
+import LogInPage from './components/LogInPage';
+import SearchPage from './components/SearchPage';
+import ProductPage from './components/ProductPage';
+import ContactPage from './components/ContactPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+       <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/new-cars" element={<NewCarsPage />} />
+        <Route path="/used-cars" element={<UsedCarsPage />} />
+        <Route path="/compare" element={<ComparePage />} />
+        <Route path="/sell" element={<SellPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/log-in" element={<LogInPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
     </div>
   );
 }
