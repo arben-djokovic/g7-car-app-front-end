7d054981b301b0990ef935b4f20ed84a925bfad8
beniiiiii <56236600+beniiiiii@users.noreply.github.com>
2022-02-09 00:21:42 +0100

    Edited Sell-Page(maked sections: Engine Details, Dimension, Features, Location, Price)

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,7 +4,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
-import {  Navigation, Pagination, Mousewheel, Keyboard  } from "swiper";
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 import '../styles/HomeStyle/HomeStyle.css'
 import Car from './Car';
 import { useNavigate } from 'react-router';
@@ -20,60 +20,61 @@ export default function HomePage() {
   let [selectedRecomendedUsed, setSelectedRecomendedUsed] = useState(false)
   const navigate = useNavigate()
   const optionsLocation = [
-    {value: 'any', label: 'any'},
-    {value: 'Podgorica', label: 'Podgorica'},
-    {value: 'Tuzi', label: 'Tuzi'},
-    {value: 'Berane', label: 'Berane'},
-    {value: 'Plav', label: 'Plav'},
-    {value: 'Petnjica', label: 'Petnjica'},
-    {value: 'Bijelo Polje', label: 'Bijelo Polje'},
-    {value: 'Kolasin', label: 'Kolasin'},
-    {value: 'Ulcinj', label: 'Ulcinj'},
-    {value: 'Niksic', label: 'Niksic'},
-    {value: 'Pljevlja', label: 'Pljevlja'},
-    {value: 'Rozaje', label: 'Rozaje'},
-    {value: 'Tivat', label: 'Tivat'},
+    { value: 'any', label: 'any' },
+    { value: 'Podgorica', label: 'Podgorica' },
+    { value: 'Tuzi', label: 'Tuzi' },
+    { value: 'Berane', label: 'Berane' },
+    { value: 'Plav', label: 'Plav' },
+    { value: 'Petnjica', label: 'Petnjica' },
+    { value: 'Bijelo Polje', label: 'Bijelo Polje' },
+    { value: 'Kolasin', label: 'Kolasin' },
+    { value: 'Ulcinj', label: 'Ulcinj' },
+    { value: 'Niksic', label: 'Niksic' },
+    { value: 'Pljevlja', label: 'Pljevlja' },
+    { value: 'Rozaje', label: 'Rozaje' },
+    { value: 'Tivat', label: 'Tivat' },
   ]
   const customStyles = {
-  option: (provided, state) => ({
-    ...provided,
-    borderBottom: '1px dotted pink',
-    color: 'white',
-    backgroundColor: '#152836',
-    padding: 20,
-    margin: 0,
-    cursor: 'pointer'
-  }),
-  menu: (provided, state) => ({
-    ...provided,
-    color: 'white'
-  }),
-  control: () => ({
-    // // none of react-select's styles are passed to <Control />
-    // width: 200,
-    display: 'flex',
-    color: 'white',
-  }),
-  singleValue: (provided, state) => {
-    const transition = 'opacity 300ms';
-    const color = 'color: white'
+    option: (provided, state) => ({
+      ...provided,
+      borderBottom: '1px dotted pink',
+      color: 'white',
+      backgroundColor: '#152836',
+      padding: 20,
+      margin: 0,
+      cursor: 'pointer'
+    }),
+    menu: (provided, state) => ({
+      ...provided,
+      color: 'white'
+    }),
+    control: () => ({
+      // // none of react-select's styles are passed to <Control />
+      // width: 200,
+      display: 'flex',
+      color: 'white',
+    }),
+    singleValue: (provided, state) => {
+      const transition = 'opacity 300ms';
+      const color = 'color: white'
 
-    return { ...provided, transition, color };
+      return { ...provided, transition, color };
+    }
   }
-}
 
   return <div className='homePage'>
     <Header />
 
     <div className="firstSection">
-      <Swiper 
-        pagination={true} 
-        modules={[Pagination]} 
-        loop={true}
-        pagination={{
-          clickable: true,
-        }} 
-        className="mySwiper">
+      <div className="swiperPc">
+        <Swiper
+          pagination={true}
+          modules={[Pagination]}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          className="mySwiper">
           <SwiperSlide>
             <div className="sliderSection">
               <img src="./assets/background-image1.png" alt="" />
@@ -111,88 +112,138 @@ export default function HomePage() {
             </div>
           </SwiperSlide>
         </Swiper>
-        <div className="searchDiv">
-          <div className="searchHeader">
-            {selectedAll ? <h2 className='searchSelected'>All</h2> : <h2 onClick={()=>{
-              setSelectedAll(true)
-              setSelectedNew(false)
-              setSelectedUsed(false)
-            }} className='searchNotSelected'>All</h2>}
-            {selectedNew ? <h2 className='searchSelected'>New</h2> : <h2 onClick={()=>{
-              setSelectedAll(false)
-              setSelectedNew(true)
-              setSelectedUsed(false)
-            }} className='searchNotSelected'>New</h2>}
-            {selectedUsed ? <h2 className='searchSelected'>Used</h2> : <h2 onClick={()=>{
-              setSelectedAll(false)
-              setSelectedNew(false)
-              setSelectedUsed(true)
-            }} className='searchNotSelected'>Used</h2>}
-          </div>
-          <div className='seconAndFirst'>
-              <div className="searchSecondDiv">
-              <div className="searchSecondDivInput">
-                <i className="fa fa-search" aria-hidden="true"></i>
-                <input placeholder='Search' type="text" />
-              </div>
-              <Select className='select' styles={customStyles} placeholder={'Models...'} options={optionsLocation} />
-              <Select className='select' styles={customStyles} placeholder={'Brands...'} options={optionsLocation} />
-            </div>
-            <div className="searchThirdDiv">
-              <div className="searchSecondDivInput">
-              <i className="fa fa-map-marker" aria-hidden="true"></i>
-              <Select className='select' styles={customStyles} placeholder={'Location...'} options={optionsLocation} />
-              </div>
-              <div className="price">
-                <div className="priceText">
-                  <div className="priceTextFirst">
-                    Price Range
-                  </div>
-                  <div className="priceTextSecond">
-                    $1000 - $3,000,000
-                  </div>
-                </div>
-                <div className="range">
-                  <h2>{'$'  + selectedRange}</h2>
-                  <input step={1000} onChange={(e)=>{setSelectedRange(e.target.value)}}  type="range" min={1000} max={3000000} />
-                </div>
-              </div>
-              <div className='submit'>Search</div>
-            </div>
-          </div>
-          
-        </div>
       </div>
 
-      <div className="recommendedCars">
-        <h2>Recommended Cars</h2>
-        <div className="recommendedHeader">
-          <div className="selecting">
-            {selectedRecomendedNew ? <h2 className='selected'>New</h2> : <h2 onClick={()=>{
-              setSelectedRecomendedNew(true)
-              setSelectedRecomendedUsed(false)
-            }} className='unSelected'>New</h2>}
-            {selectedRecomendedUsed ? <h2 className='selected'>Used</h2> : <h2 onClick={()=>{
-              setSelectedRecomendedNew(false)
-              setSelectedRecomendedUsed(true)
-            }} className='unSelected'>Used</h2>}
-          </div>
-          {selectedRecomendedUsed ? <p onClick={()=>{
-            navigate('/used-cars')
-          }}>{'See more >'}</p> : <p onClick={()=>{
-            navigate('/new-cars')
-          }}>{'See more >'}</p>}
+      <div className="swiperPhone">
+        <Swiper
+          pagination={true}
+          modules={[Pagination]}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          className="mySwiper">
+          <SwiperSlide>
+            <div className="sliderSection">
+              <img src="./assets/background-image-tel1.jpg" alt="" />
+              <div className="textSlider">
+                <h1>Find your dream car</h1>
+                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="sliderSection">
+              <img src="./assets/background-image-tel2.jpg" alt="" />
+              <div className="textSlider">
+                <h1>Find your dream car</h1>
+                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="sliderSection">
+              <img src="./assets/background-image-tel3.jpg" alt="" />
+              <div className="textSlider">
+                <h1>Find your dream car</h1>
+                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="sliderSection">
+              <img src="./assets/background-image-tel4.jpg" alt="" />
+              <div className="textSlider">
+                <h1>Find your dream car</h1>
+                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+
+      <div className="searchDiv">
+        <div className="searchHeader">
+          {selectedAll ? <h2 className='searchSelected'>All</h2> : <h2 onClick={() => {
+            setSelectedAll(true)
+            setSelectedNew(false)
+            setSelectedUsed(false)
+          }} className='searchNotSelected'>All</h2>}
+          {selectedNew ? <h2 className='searchSelected'>New</h2> : <h2 onClick={() => {
+            setSelectedAll(false)
+            setSelectedNew(true)
+            setSelectedUsed(false)
+          }} className='searchNotSelected'>New</h2>}
+          {selectedUsed ? <h2 className='searchSelected'>Used</h2> : <h2 onClick={() => {
+            setSelectedAll(false)
+            setSelectedNew(false)
+            setSelectedUsed(true)
+          }} className='searchNotSelected'>Used</h2>}
         </div>
-        <div className="recommendedNewCarsPc">
-          {selectedRecomendedUsed ? <><Car isNew={false} />
+        <div className='seconAndFirst'>
+          <div className="searchSecondDiv">
+            <div className="searchSecondDivInput">
+              <i className="fa fa-search" aria-hidden="true"></i>
+              <input placeholder='Search' type="text" />
+            </div>
+            <Select className='select' styles={customStyles} placeholder={'Models...'} options={optionsLocation} />
+            <Select className='select' styles={customStyles} placeholder={'Brands...'} options={optionsLocation} />
+          </div>
+          <div className="searchThirdDiv">
+            <div className="searchSecondDivInput">
+              <i className="fa fa-map-marker" aria-hidden="true"></i>
+              <Select className='select' styles={customStyles} placeholder={'Location...'} options={optionsLocation} />
+            </div>
+            <div className="price">
+              <div className="priceText">
+                <div className="priceTextFirst">
+                  Price Range
+                </div>
+                <div className="priceTextSecond">
+                  $1000 - $3,000,000
+                </div>
+              </div>
+              <div className="range">
+                <h2>{'$' + selectedRange}</h2>
+                <input step={1000} onChange={(e) => { setSelectedRange(e.target.value) }} type="range" min={1000} max={3000000} />
+              </div>
+            </div>
+            <div className='submit'>Search</div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div className="recommendedCars">
+      <h2>Recommended Cars</h2>
+      <div className="recommendedHeader">
+        <div className="selecting">
+          {selectedRecomendedNew ? <h2 className='selected'>New</h2> : <h2 onClick={() => {
+            setSelectedRecomendedNew(true)
+            setSelectedRecomendedUsed(false)
+          }} className='unSelected'>New</h2>}
+          {selectedRecomendedUsed ? <h2 className='selected'>Used</h2> : <h2 onClick={() => {
+            setSelectedRecomendedNew(false)
+            setSelectedRecomendedUsed(true)
+          }} className='unSelected'>Used</h2>}
+        </div>
+        {selectedRecomendedUsed ? <p onClick={() => {
+          navigate('/used-cars')
+        }}>{'See more >'}</p> : <p onClick={() => {
+          navigate('/new-cars')
+        }}>{'See more >'}</p>}
+      </div>
+      <div className="recommendedNewCarsPc">
+        {selectedRecomendedUsed ? <><Car isNew={false} />
           <Car isNew={false} />
           <Car isNew={false} /></> : <><Car isNew={true} />
           <Car isNew={true} />
           <Car isNew={true} /></>}
-          
-        </div>
-        <div className="recommendedNewCarMobile">
-          <Swiper
+
+      </div>
+      <div className="recommendedNewCarMobile">
+        <Swiper
           slidesPerView={1}
           spaceBetween={30}
           loop={true}
@@ -202,106 +253,106 @@ export default function HomePage() {
           navigation={true}
           modules={[Pagination, Navigation]}
           className="mySwiper"
-          >
-            {selectedRecomendedUsed ? <><SwiperSlide><Car isNew={false} /></SwiperSlide>
+        >
+          {selectedRecomendedUsed ? <><SwiperSlide><Car isNew={false} /></SwiperSlide>
             <SwiperSlide><Car isNew={false} /></SwiperSlide>
             <SwiperSlide><Car isNew={false} /></SwiperSlide></> : <><SwiperSlide><Car isNew={true} /></SwiperSlide>
             <SwiperSlide><Car isNew={true} /></SwiperSlide>
             <SwiperSlide><Car isNew={true} /></SwiperSlide></>}
-            
-          </Swiper>
+
+        </Swiper>
+      </div>
+    </div>
+
+    <div className="compareCars">
+      <h2>Compare Cars</h2>
+      <div className="compareCarsSecond">
+        <Car isNew={true} />
+        <div className="versus">VS</div>
+        <Car isNew={true} />
+      </div>
+      <div onClick={() => { navigate('compare') }} className="compareBtn">
+        <h2>Compare Car</h2>
+      </div>
+    </div>
+
+    <div className="aboutUs">
+      <img src="./assets/about-us-car.png" alt="" />
+      <div className="text">
+        <div className="textFirst">
+          <h2>About Us</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel aliquet tortor ut sit sit. Velit imperdiet integer elementum a scelerisque pulvinar venenatis sodales. Quis nulla euismod feugiat at interdum in. Venenatis arcu semper lectus quis sit in rhoncus auctor.</p>
         </div>
-      </div>
-
-      <div className="compareCars"> 
-          <h2>Compare Cars</h2>
-          <div className="compareCarsSecond">
-            <Car isNew={true} />
-            <div className="versus">VS</div>
-            <Car isNew={true} />
+        <div className="textSecond">
+          <div>
+            <h2>150</h2>
+            <p>Vehicle In Stock</p>
           </div>
-          <div onClick={()=>{navigate('compare')}} className="compareBtn">
-            <h2>Compare Car</h2>
+          <div>
+            <h2>40</h2>
+            <p>Sold Car</p>
           </div>
-      </div>
-
-      <div className="aboutUs">
-        <img src="./assets/about-us-car.png" alt="" />
-        <div className="text">
-          <div className="textFirst">
-            <h2>About Us</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel aliquet tortor ut sit sit. Velit imperdiet integer elementum a scelerisque pulvinar venenatis sodales. Quis nulla euismod feugiat at interdum in. Venenatis arcu semper lectus quis sit in rhoncus auctor.</p>
+          <div>
+            <h2>38</h2>
+            <p>Happy Customer</p>
           </div>
-          <div className="textSecond">
-            <div>
-              <h2>150</h2>
-              <p>Vehicle In Stock</p>
-            </div>
-            <div>
-              <h2>40</h2>
-              <p>Sold Car</p>
-            </div>
-            <div>
-              <h2>38</h2>
-              <p>Happy Customer</p>
-            </div>
-            <div>
-              <h2>5</h2>
-              <p>Awards</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="ourService">
-        <h2>Our Service</h2>
-        <div className="second">
-          <div onClick={()=>{navigate('new-cars')}}>
-            <img src="./assets/buy-car-icon.png" alt="" />
-            <p>Buy a new car</p>
-          </div>
-          <div onClick={()=>{navigate('used-cars')}}>
-            <img src="./assets/buy-used-car-icon.png" alt="" />
-            <p>Buy an used car</p>
-          </div>
-          <div onClick={()=>{navigate('sell')}}>
-            <img src="./assets/sell-car-icon.png" alt="" />
-            <p>Sell my car</p>
+          <div>
+            <h2>5</h2>
+            <p>Awards</p>
           </div>
         </div>
       </div>
+    </div>
 
-      <div className="contactForm">
-        <h2>Contact</h2>
-        <div className="form">
-          <div>
-            <p>Name</p>
-            <input placeholder='Full name' type="name" />
-          </div>
-          <div>
-            <p>Email</p>
-            <input placeholder='email@gmail.com' type="email" />
-          </div>
-          <div>
-            <p>Phone</p>
-            <input placeholder='000-000-000' type="tel" />
-          </div>
-          <div>
-            <p>Comment</p>
-            <textarea placeholder='Leave a message here' name="" id="" cols="30" rows="10"></textarea>
-          </div>
+    <div className="ourService">
+      <h2>Our Service</h2>
+      <div className="second">
+        <div onClick={() => { navigate('new-cars') }}>
+          <img src="./assets/buy-car-icon.png" alt="" />
+          <p>Buy a new car</p>
+        </div>
+        <div onClick={() => { navigate('used-cars') }}>
+          <img src="./assets/buy-used-car-icon.png" alt="" />
+          <p>Buy an used car</p>
+        </div>
+        <div onClick={() => { navigate('sell') }}>
+          <img src="./assets/sell-car-icon.png" alt="" />
+          <p>Sell my car</p>
+        </div>
+      </div>
+    </div>
+
+    <div className="contactForm">
+      <h2>Contact</h2>
+      <div className="form">
+        <div>
+          <p>Name</p>
+          <input placeholder='Full name' type="name" />
+        </div>
+        <div>
+          <p>Email</p>
+          <input placeholder='email@gmail.com' type="email" />
+        </div>
+        <div>
+          <p>Phone</p>
+          <input placeholder='000-000-000' type="tel" />
+        </div>
+        <div>
+          <p>Comment</p>
+          <textarea placeholder='Leave a message here' name="" id="" cols="30" rows="10"></textarea>
+        </div>
         <p className="sendBtn">Send</p>
-        </div>
       </div>
+    </div>
 
-      <div className="sponsors">
-        <img src="./assets/tesla-logo.png" alt="" />
-        <img src="./assets/audi-logo.png" alt="" />
-        <img src="./assets/fiat-logo.png" alt="" />
-        <img src="./assets/huandai-logo.png" alt="" />
-        <img src="./assets/peugeot-logo.png" alt="" />
-        <img src="./assets/volvo-logo.png" alt="" />
-      </div>
-      <Footer />
+    <div className="sponsors">
+      <img src="./assets/tesla-logo.png" alt="" />
+      <img src="./assets/audi-logo.png" alt="" />
+      <img src="./assets/fiat-logo.png" alt="" />
+      <img src="./assets/huandai-logo.png" alt="" />
+      <img src="./assets/peugeot-logo.png" alt="" />
+      <img src="./assets/volvo-logo.png" alt="" />
+    </div>
+    <Footer />
   </div>;
 }
--- src/components/SellPage.js
+++ src/components/SellPage.js
@@ -6,10 +6,10 @@ import Select from 'react-select'
 
 export default function SellPage() {
   const options = [
-    { value: 'Tuzi', label: 'Tuzi' },
-    { value: 'Berane', label: 'Berane' },
-    { value: 'Plav', label: 'Plav' },
-    { value: 'Petnjica', label: 'Petnjica' }
+    { value: '1', label: 'Opcija 1' },
+    { value: '2', label: 'Opcija 2' },
+    { value: '3', label: 'Opcija 3' },
+    { value: '4', label: 'Opcija 4' }
   ]
   const customStyles = {
     option: (provided, state) => ({
@@ -38,7 +38,8 @@ export default function SellPage() {
       return { ...provided, transition, color };
     }
   }
-  let [passengerCapacity, setPassengerCapacity ] = useState(2)
+  let [passengerCapacity, setPassengerCapacity] = useState(2)
+
   return <div>
     <Header />
     <div className="sellPage">
@@ -89,15 +90,15 @@ export default function SellPage() {
           <div className="section7">
             <p>Passenger Capacity</p>
             <div className="counter">
-              <i onClick={()=>{
-                if(passengerCapacity != 0){
+              <i onClick={() => {
+                if (passengerCapacity != 0) {
                   setPassengerCapacity(passengerCapacity => passengerCapacity - 1)
                 }
-              }} class="fa fa-minus" aria-hidden="true"></i>
+              }} className="fa fa-minus" aria-hidden="true"></i>
               <div className="counterNumber">{passengerCapacity}</div>
-              <i onClick={()=>{
+              <i onClick={() => {
                 setPassengerCapacity(passengerCapacity => passengerCapacity + 1)
-              }} class="fa fa-plus" aria-hidden="true"></i>
+              }} className="fa fa-plus" aria-hidden="true"></i>
             </div>
           </div>
           <div className="section8">
@@ -109,8 +110,187 @@ export default function SellPage() {
             <textarea maxLength={250} placeholder='Write description about your car' className='textArea' id="" cols="30" rows="8"></textarea>
           </div>
         </div>
-
       </div>
+
+      <div className="engineDetails">
+        <h2>Engine Details</h2>
+        <div className="mainSecion">
+          <div className='first'>
+            <p>Fuel Type</p>
+            <Select className='select' styles={customStyles} options={options} />
+          </div>
+          <div className='mid'>
+            <p>Mileage</p>
+            <div className="inputDiv">
+              <input type="number" name="" id="" />
+              <p>km</p>
+            </div>
+          </div>
+          <div className='last'>
+            <p>Transmission</p>
+            <Select className='select' styles={customStyles} options={options} />
+          </div>
+          <div className='first'>
+            <p>Drivetrain</p>
+            <Select className='select' styles={customStyles} options={options} />
+          </div>
+          <div className='mid'>
+            <p>Engine Capacity</p>
+            <div className="inputDiv">
+              <input type="number" name="" id="" />
+              <p>km</p>
+            </div>
+          </div>
+          <div className='last'>
+            <p>Power</p>
+            <div className="inputDiv">
+              <input type="number" name="" id="" />
+              <p>km</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="dimensions">
+        <h2>Dimensions</h2>
+        <div className="dimensionSection">
+          <div className="inputDiv">
+            <p>Length</p>
+            <div>
+              <input type="number" />
+              <p>mm</p>
+            </div>
+          </div>
+          <div className="inputDiv">
+            <p>Width</p>
+            <div>
+              <input type="number" />
+              <p>mm</p>
+            </div>
+          </div>
+          <div className="inputDiv">
+            <p>Height</p>
+            <div>
+              <input type="number" />
+              <p>mm</p>
+            </div>
+          </div>
+          <div className="inputDiv">
+            <p>Cargo Volume</p>
+            <div>
+              <input type="number" />
+              <p>L</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="features">
+        <h2>Features</h2>
+        <div className="checkList">
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="11" />
+            <label htmlFor="11">
+              <p>Power Steering</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="12" />
+            <label htmlFor="12">
+              <p>Heated Seats</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="13" />
+            <label htmlFor="13">
+              <p>Rear Parking Sensor</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="14" />
+            <label htmlFor="14">
+              <p>USB Port</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="15" />
+            <label htmlFor="15">
+              <p>AC</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="16" />
+            <label htmlFor="16">
+              <p>Wifi</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="17" />
+            <label htmlFor="17">
+              <p>Roof Rack</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="18" />
+            <label htmlFor="18">
+              <p>Sound System</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="19" />
+            <label htmlFor="19">
+              <p>Alarm</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="20" />
+            <label htmlFor="20">
+              <p>Cruise Control</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="21" />
+            <label htmlFor="21">
+              <p>Power Windows</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="22" />
+            <label htmlFor="22">
+              <p>Memory Seat</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="23" />
+            <label htmlFor="23">
+              <p>Bluetooth</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="24" />
+            <label htmlFor="24">
+              <p>Front Parking Sensor</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="25" />
+            <label htmlFor="25">
+              <p>Sunroof</p>
+            </label>
+          </div>
+          <div className="checkDiv"><input type="checkbox" name="featuresCheckbox" id="26" />
+            <label htmlFor="26">
+              <p>Other</p>
+            </label>
+          </div>
+        </div>
+        <textarea placeholder='Write another feature here' className='textArea' name="" id="" cols="30" rows="10"></textarea>
+      </div>
+
+      <div className="location">
+        <h2>Location</h2>
+        <div className="content">
+          <p>Adress:</p>
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="price">
+        <h2>Price</h2>
+        <div className="content">
+          <p>Full Price:</p>
+          <div className="input">
+            <p>$</p>
+            <input type="text" />
+          </div>
+        </div>
+      </div>
+
     </div>
     <Footer />
   </div>;
